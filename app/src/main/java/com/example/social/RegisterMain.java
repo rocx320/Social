ed372d743f4d2dad8657be00f2e57bee8ebc9bd0
Piyush <rocx320@gmail.com>
2023-08-23 01:14:11 +0530

    login and register

--- app/src/main/java/com/example/social/RegisterMain.java
+++ app/src/main/java/com/example/social/RegisterMain.java
@@ -3,19 +3,40 @@ package com.example.social;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterMain extends AppCompatActivity {
-
-    private EditText email,password;
-    private Button register;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_main);
+
+        EditText email = findViewById(R.id.signup_email);
+        EditText password = findViewById(R.id.signup_password);
+        Button register = findViewById(R.id.signup_btn);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String txt_email = email.getText().toString();
+                String txt_pass = password.getText().toString();
+
+                if(TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_pass)){
+
+                    Toast.makeText(RegisterMain.this, "Empty Credentials", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+
+
 
 
     }
